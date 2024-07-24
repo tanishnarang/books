31adb182cbf99bdf2a126bea5adac0f2b0e7618f
Tanish Narang <narangtanish2@gmail.com>
2024-07-24 21:42:00 +0530

    delete books by json in action

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ export default function App() {
     fetchBooks();
   }, []);
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
